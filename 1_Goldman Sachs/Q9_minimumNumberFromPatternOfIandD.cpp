3ff6789e6c1c903c0e83dd0ce8f586b009f73987
Mohit Singh <mohit.s0631@gmail.com>
2022-01-20 05:13:34 +0530

    Create Q9_minimumNumberFromPatternOfIandD.cpp

---
+++ 1_Goldman Sachs/Q9_minimumNumberFromPatternOfIandD.cpp
@@ -0,0 +1,41 @@
+string ans = ""; // Minimum number following pattern
+ 
+    int i = 0;
+    int cur = 1; // cur val following pattern
+    int dCount = 0; // Count of char 'D'
+    while (i < str.length()) {
+ 
+        char ch = str[i];
+ 
+        // If 1st ch == 'I', incr and add to ans
+        if (i == 0 && ch == 'I') {
+            ans += to_string(cur);
+            cur++;
+        }
+ 
+        // If cur char == 'D',
+        // incr dCount as well, since we always
+        // start counting for dCount from i+1
+        if (ch == 'D') {
+            dCount++;
+        }
+ 
+        int j = i + 1; // Count 'D' from i+1 index
+        while (j < str.length()
+               && str[j] == 'D') {
+            dCount++;
+            j++;
+        }
+ 
+        int k = dCount;  // Store dCount
+        while (dCount >= 0) {
+            ans += to_string(cur + dCount);
+            dCount--;
+        }
+ 
+        cur += (k + 1); // Manages next cur val
+        dCount = 0;
+        i = j;
+    }
+ 
+    return ans;
